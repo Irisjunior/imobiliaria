d9f386eda1bf2b617ab3c062188b12e6430d32bf
Iris Leite Vieira Junior <irisjunior1@gmail.com>
2021-05-14 11:59:30 -0300

    add script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+  imoveis = [
+  {name:"mansao cidade",  valor:650.000,tipo:venda,   banheiros:2, quartos:3},
+  {name:"mansao praia",   valor:480.000,tipo:aluguel, banheiros:2, quartos:2},
+  {name:"mansao ilha",    valor:470.000,tipo:venda,   banheiros:2, quartos:3},
+  {name:"chale campo",    valor:400.000,tipo:aluguel, banheiros:2, quartos:4},
+  {name:"chale montanha", valor:450.000,tipo:venda,   banheiros:2, quartos:3},
+  {name:"mansao centro",  valor:650.000,tipo:aluguel, banheiros:2, quartos:2}
+]
+localStorage.setItem(imoveis);
+
+
